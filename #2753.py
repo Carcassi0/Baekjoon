e5a5dc85c7bfe27b5d4e9a70b1030015d2dab04e
WEIUIJONG <dnldmlwhd@ajou.ac.kr>
2024-02-25 12:00:46 +0900

    윤년

--- #2753.py
+++ #2753.py
@@ -5,7 +5,7 @@ year = int(input())
 def leapYear(year):
     if(year%4 == 0 and year%100 != 0):
         print(1)
-    if(year%4 == 0 and year%400 == 0):
+    elif(year%4 == 0 and year%400 == 0):
         print(1)
     else:
         print(0)
