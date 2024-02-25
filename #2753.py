b67f7cea2d433c4851079d290a647bb43b751bb0
WEIUIJONG <dnldmlwhd@ajou.ac.kr>
2024-02-25 12:00:19 +0900

    윤년

---
+++ #2753.py
@@ -0,0 +1,13 @@
+#2753
+
+year = int(input())
+
+def leapYear(year):
+    if(year%4 == 0 and year%100 != 0):
+        print(1)
+    if(year%4 == 0 and year%400 == 0):
+        print(1)
+    else:
+        print(0)
+
+leapYear(year)
